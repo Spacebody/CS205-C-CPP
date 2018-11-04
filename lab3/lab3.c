3144f4e9603d8f4f81432d86524fb1ac40893ff1
Spacebody <510662916@qq.com>
2018-11-05 01:32:32 +0800

    Fix bug in lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -5,37 +5,40 @@
 #include "utf8.h"
 
 #define STR_LEN 1024
-#define ARR_SIZE 300
+#define ARR_SIZE 300  // enough size
 #define BUF_SIZE 20480
 
+// definition of language blocks
 typedef struct Block
 {
-    unsigned int head;
-    unsigned int tail;
-    char name[STR_LEN];
+    unsigned int head;  // begining
+    unsigned int tail;  // end
+    char name[STR_LEN];  // block name
 }Block;
 
+Block blocks[ARR_SIZE];
+int count[ARR_SIZE] = {0};  // count the languages
 
-Block blocks[ARR_SIZE]; 
-int count[ARR_SIZE] = {0};
-
-void read_blocks(const char* file_name);
-int search(int codepoint);
-char* major_block(void);
+void read_blocks(const char* file_name);  // read block files
+int search(int codepoint);  // search block which the character belongs to
+char* major_block(void);  // find the most appeared language
 
 int main(int argc, char const *argv[])
 {
     read_blocks("Blocks.txt");
     char buffer[BUF_SIZE];
-    fgets(buffer, BUF_SIZE, stdin);
-    unsigned char *ptr;
-    ptr = (unsigned char *)buffer;
-    int lengptr = 0;
-    do
+    while(fgets(buffer, BUF_SIZE, stdin))  // read from stdin
     {
-        search(utf8_to_codepoint(ptr, &lengptr));
-        ptr += lengptr;
-    } while (*(ptr-1) == '\0');
+        unsigned char *ptr;
+        ptr = (unsigned char *)buffer;
+        int lenptr = 0;
+        while (*ptr != '\0')  // loop until reach the end of the line
+        {
+            search(utf8_to_codepoint(ptr, &lenptr));
+            ptr += lenptr;
+        }
+        memset(buffer, '\0', BUF_SIZE);  // reset array
+    }
     fprintf(stdout, "%s\n", major_block());
     return 0;
 }
@@ -44,7 +47,7 @@ void read_blocks(const char* file_name)
 {
     FILE *file = fopen(file_name, "r");
     if(file == NULL)
-    {
+    {   // if file is not found...
         fputs("Error: No such file!\n", stderr);
     }
     else
@@ -53,7 +56,7 @@ void read_blocks(const char* file_name)
         int index;
         int num = 0;
         for (index = 0; !feof(file) && !ferror(file) && fgets(line, STR_LEN, file); ++index, \
-                memset(line, 0, sizeof(STR_LEN)))
+                memset(line, 0, STR_LEN))
         {
             if (line[0] == '#' || line[0] == ' ' || line[0] == '\n')
             {
@@ -62,18 +65,17 @@ void read_blocks(const char* file_name)
             Block new_block;
             char* rest = NULL;
             char* token = NULL;
-            token = strtok_r(line, ".; ", &rest);
+            token = strtok_r(line, ".; ", &rest);  // get head
             sscanf(token, "%x", &new_block.head);
-            token = strtok_r(rest, ".; ", &rest);
+            token = strtok_r(rest, ".; ", &rest);  // get tail
             sscanf(token, "%x", &new_block.tail);
             rest[strlen(rest)-1] = '\0';
             ++rest;
-            strcpy(new_block.name, rest);
-            blocks[num++] = new_block;
+            strcpy(new_block.name, rest);  // get block name
+            blocks[num++] = new_block;  // store blocks
         }
     }
-    fclose(file);
-    // fputs("Read complete!\n", stdout);
+    fclose(file);  // close file
 }
 
 int search(int codepoint)
@@ -82,7 +84,7 @@ int search(int codepoint)
     for(i = 0; i < ARR_SIZE; ++i)
     {
         if(codepoint >= blocks[i].head && codepoint <= blocks[i].tail)
-        {
+        {   // if codepoint is within the range of certain language block, increase the counter
             index = i;
             ++(count[index]);
             break;
@@ -97,7 +99,7 @@ char* major_block(void)
     int num = 0;
     int i;   
     for(i = 0; i < ARR_SIZE; ++i)
-    {
+    {   // find index of the most appeared language
         if(count[i] > num)
         {
             num = count[i];
@@ -106,7 +108,7 @@ char* major_block(void)
     }
     if (index_of_majority != -1)
     {
-        return blocks[index_of_majority].name;
+        return blocks[index_of_majority].name;  //return the name
     }
     return "";
 }
